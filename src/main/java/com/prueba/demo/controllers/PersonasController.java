d5e3428de322ea180ad8b179ad40441d9e5ab844
Andres Castellanos <andresrobanesky@gmail.com>
2021-05-15 20:25:08 -0500

    Cors Filter Globales

--- src/main/java/com/prueba/demo/controllers/PersonasController.java
+++ src/main/java/com/prueba/demo/controllers/PersonasController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api/servicios")
 public class PersonasController {
@@ -14,30 +15,25 @@ public class PersonasController {
     private PersonasServices personasServices;
 
 
-    @CrossOrigin(origins = "*")
     @GetMapping("/personas")
     public List<PersonasEntity> getPersonas() {
         return personasServices.getPersonas();
     }
 
-    @CrossOrigin(origins = "*")
     @GetMapping("/personasporapellido/{apellido}")
     public List<PersonasEntity> getPersonasApellido(@PathVariable(value = "apellido") String apellido) {
         return personasServices.getPersonasPorApellido(apellido);
     }
-    @CrossOrigin(origins = "*")
     @PostMapping("/personas")
     public PersonasEntity addPersona(@RequestBody PersonasEntity pers) {
         return personasServices.crearEdPersonas(pers);
     }
 
-    @CrossOrigin(origins = "*")
     @PutMapping("/personas")
     public PersonasEntity editPersona(@RequestBody PersonasEntity pers) {
         return personasServices.crearEdPersonas(pers);
     }
 
-    @CrossOrigin(origins = "*")
     @DeleteMapping("/personas/{id}")
     public Integer elimPersona(@PathVariable(value = "id") Long IdPers) {
         return personasServices.elimPersona(IdPers);
